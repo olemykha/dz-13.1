01a3183e603195bf1871b376e3fd7be986a4d8c4
olemykha <alex.mkhlk@gmail.com>
2023-06-14 12:20:46 +0000

    add homework13

---
+++ src/main/java/org/example/TestModel.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class TestModel {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public TestModel(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
